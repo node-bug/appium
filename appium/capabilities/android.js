a88fa7a444241fefa05e22175833974c9ebb6d4e
Mahmoud Abuzaid <mahmoud.abuzaid@uniscon.com>
2025-03-11 14:53:46 +0100

    adding android capabilities

--- appium/capabilities/android.js
+++ appium/capabilities/android.js
@@ -1,11 +1,16 @@
 class Android {
   static capabilities(server, capability) {
+    const deviceName = capability.deviceName || 'pixel_6'
+    const platformVersion = capability.platformVersion || '12.0'
+    const automationName = capability.platformVersion || 'UiAutomator2'
+    const app = capability.app || ''
+
     return {
-      platformName: capability.platformName,
-      'appium:deviceName': capability.deviceName,
-      'appium:platformVersion': capability.platformVersion,
-      'appium:automationName': capability.automationName,
-      'appium:app': capability.app,
+      platformName: 'Android',
+      'appium:deviceName': deviceName,
+      'appium:platformVersion': platformVersion,
+      'appium:automationName': automationName,
+      'appium:app': app,
       'appium:enforceXPath1': true,
     }
   }
